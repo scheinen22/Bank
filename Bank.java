b4afc31b3912529d56b78e32b147432f0edd0fca
scheinen22 <scheinenabi@gmail.com>
2024-10-01 11:41:05 +0200

    Bank.java

--- Bank.java
+++ Bank.java
@@ -3,31 +3,39 @@ package banksystem;
 public class Bank {
 
     private int blz;
-    private int eckartennummer;
+    private String bankname;
 
     public Bank() {
-        System.out.println("Die Banksystem.Bank wurde gesetzt.");
-    }
-
-    public Bank(int blz, int eckartennummer) {
-        this.blz = blz;
-        this.eckartennummer = eckartennummer;
-    }
-
-    public int getEckartennummer() {
-        return eckartennummer;
 
     }
-
+    public Bank(int blz, String bankname) {
+        this.setBlz(blz);
+        this.setBankname(bankname);
+    }
+    public String getBankname() {
+        return bankname;
+    }
     public int getBlz() {
         return blz;
     }
-
     public void setBlz(int blz) {
         this.blz = blz;
     }
-
-    public void setEckartennummer(int eckartennummer) {
-        this.eckartennummer = eckartennummer;
+    public void setBankname(String bankname) {
+        this.bankname = bankname;
+    }
+    public String bankinformationenAnzeigenString() {
+        return "BLZ: " + this.getBlz() + "\nBankname: " + this.getBankname();
+    }
+    public void transfer(Konto sender, Konto empfaenger, double betrag, int blz) {
+        if (empfaenger.getBank().getBlz() != blz) {
+            System.out.println("Überweisung fehlgeschlagen.\nBankleitzahl stimmt nicht überein.");
+            return;
+        }
+        if (sender.transaktion(betrag, empfaenger, blz)) {
+            System.out.println("Überweisung von Konto " + sender.getIban() + " zu Konto " + empfaenger.getIban() + " abgeschlossen.\n");
+        } else {
+            System.out.println("Überweisung fehlgeschlagen.");
+        }
     }
 }
