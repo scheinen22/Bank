ee2237b244d95522c411e88d836f6da72ce9720f
scheinen22 <scheinenabi@gmail.com>
2024-10-01 13:44:44 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,8 +6,7 @@ public class Main {
         Konto konto = new Konto(new Bank(12345, "Sparkasse"), 432323, 28923.12, new Kunde("Jochen", "Schmidt", "21.12.2000", "Teststraße"));
         Konto konto2 = new Konto(new Bank(123456, "Sparkasse"), 2131, 31331, new Kunde("Kai", "Humboldt", "12.10.2000", "Bevingsweg"));
         Konto konto3 = new Konto(new Bank(123,"Deutsche Bank"), 9876, 1999, new Kunde("Manuel", "Hammer", "16.04.1987", "Grafstraße"));
-        //bank.transfer(konto, konto2, 2000, 12345);
-        bank.transfer(konto2, konto, 1832, 12345);
+        bank.transfer(konto2, konto, 1832, 12345, 432323);
         konto.printTransaktionen();
         konto3.abheben(213);
         System.out.println(konto3.getKontostand());
