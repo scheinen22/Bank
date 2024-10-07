d289b834b435ed87a6439c386d7093cf5335d824
scheinen22 <scheinenabi@gmail.com>
2024-10-07 08:46:49 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,9 +3,9 @@ package banksystem;
 public class Main {
     public static void main(String[] args) {
         Bank bank = new Bank();
-        Konto konto = new Konto(new Bank(12345, "Sparkasse"), 432323, 28923.12, 2000, new Kunde("Jochen", "Schmidt", "21.12.2000", "Teststraße"));
-        Konto konto2 = new Konto(new Bank(123456, "Sparkasse"), 2131, 31331.32, 2000, new Kunde("Kai", "Humboldt", "12.10.2000", "Bevingsweg"));
-        Konto konto3 = new Konto(new Bank(123,"Deutsche Bank"), 9876, 0, 2000, new Kunde("Manuel", "Hammer", "16.04.1987", "Grafstraße"));
+        Konto konto = new Konto(new Bank(12345, "Sparkasse"), 432323, 28923.12, 2000, 2000, new Kunde("Jochen", "Schmidt", "21.12.2000", "Teststraße"));
+        Konto konto2 = new Konto(new Bank(123456, "Sparkasse"), 2131, 31331.32, 2000, 2000, new Kunde("Kai", "Humboldt", "12.10.2000", "Bevingsweg"));
+        Konto konto3 = new Konto(new Bank(123,"Deutsche Bank"), 9876, 0, 2000, 2000, new Kunde("Manuel", "Hammer", "16.04.1987", "Grafstraße"));
         // Überweisung geht problemlos durch.
         bank.transfer(konto, konto2, 1832, 123456, 2131);
         // Überweisungslimit wird überschritten (2000€)
@@ -23,6 +23,6 @@ public class Main {
         for(int i = 0; i <= 4; i++) {
             bank.transfer(konto2, konto, 15, 12345, 432323);
         }
-        konto2.printTransaktionen();
+        konto2.transaktionenString();
     }
 }
